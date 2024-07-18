c12e32d19d562d9931c6a85c67dddd8e386a56b0
aksnd <88372237+aksnd@users.noreply.github.com>
2024-07-18 22:32:24 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+// src/App.js
+
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleGetRequest = () => {
+    fetch(`http://127.0.0.1:8000/simple-text/${inputValue}/`)
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+      .catch(error => console.error('Error:', error));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Message from Django API</h1>
+      
+      <input 
+        type="text" 
+        value={inputValue} 
+        onChange={e => setInputValue(e.target.value)} 
+        placeholder="Enter a value" 
+      />
+      <button onClick={handleGetRequest}>Send GET Request</button>
+      <p>{message}</p>
     </div>
   );
 }
