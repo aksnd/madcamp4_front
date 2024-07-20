b210b77f594796ac953f333e3f6b3da2cb613366
seilj <been22026@gmail.com>
2024-07-20 14:56:39 +0900

    pages added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import StockPredictor from './StockPredictor';
-import MainPage from './MainPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import GlobalStyle from './globalStyles';
+import MainPage from './pages/MainPage';
+import CompanyPage from './pages/CompanyPage';
+import ChatbotPage from './pages/ChatbotPage';
+import StockPredictor from './pages/StockPredictor';
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/predict">Stock Predictor</Link>
-            </li>
-            <li>
-              <Link to="/mainpage">Hello World</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/predict" element={<StockPredictor />} />
-          <Route path="/mainPage" element={<MainPage />} />
-        </Routes>
-      </div>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/company" element={<CompanyPage />} />
+        <Route path="/chatbot" element={<ChatbotPage />} />
+        <Route path="/predictor" element={<StockPredictor />} />
+      </Routes>
     </Router>
   );
 }
